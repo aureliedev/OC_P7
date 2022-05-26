447771db6f7b38a3563b93537515962169a0c243
Aurelie Menin <atypik77@hotmail.fr>
2022-05-26 12:31:05 +0200

    config mongoDB ok

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
+/******************** CONFIGURATION DE L'APPLICATION EXPRESS********************/
+/* IMPORT */
 const express = require ('express');
 const app = express();
-//config dotenv
-require('dotenv').config({path: './config/.env'});
+const mongoose = require('mongoose')
 
-// EXPORT
+/* CONFIG DOTENV */
+require('dotenv').config({path: './config/.env'});
+const username = process.env.dbUserName;
+const password = process.env.dbPassword;
+const db = process.env.DB_NAME;
+
+/* Connexion à la base de données MongoDB */
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.ndndt.mongodb.net/${db}?retryWrites=true&w=majority`)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+/* EXPORT */ 
 module.exports = app;
+
