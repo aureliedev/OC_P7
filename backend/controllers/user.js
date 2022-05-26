79046f581dcf77cf2749b1f38a211d7ef8a4c9e8
Aurelie Menin <atypik77@hotmail.fr>
2022-05-26 16:47:34 +0200

    fonction getOneUser test postman ok

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,9 +1,21 @@
-/* IMPORT */ 
+/* IMPORT */
 const UserModel = require("../models/user");
 const ObjectID = require("mongoose").Types.ObjectId;
 
-/*----------------- GETALLUSERS ---------------*/ 
+/*----------------- getAllUsers ---------------*/
 module.exports.getAllUsers = async (req, res) => {
-    const users = await UserModel.find().select('-password'); //'-password n'envoie pas le MDP a la db
-    res.status(200).json(users);
-  };
+  const users = await UserModel.find().select("-password"); //'-password n'envoie pas le MDP a la db
+  res.status(200).json(users);
+};
+
+/*----------------- getOneUser ---------------*/
+module.exports.getOneUser = (req, res) => {
+  console.log(req.params);
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID inconnu : " + req.params.id);
+
+  UserModel.findById(req.params.id, (err, data) => {
+    if (!err) res.send(data);
+    else console.log("ID inconnu : " + err);
+  }).select("-password");
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,10 +4,11 @@ const router = require("express").Router();
 const authController = require("../controllers/auth");
 const userController = require("../controllers/user");
 
-/* AUTH */
-router.post("/signup", authController.signUp);
+/* ROUTES */
+router.post("/signup", authController.signUp); // auth signup
 /* USER */
-router.get("/", userController.getAllUsers);
+router.get("/", userController.getAllUsers); // tous les users
+router.get("/:id", userController.getOneUser); // Un user
 
 /* EXPORT */ 
 module.exports = router;
