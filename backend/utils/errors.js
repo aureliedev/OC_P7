808dfece1028de0e0051194bfbc085d50d93697a
Aurelie Menin <atypik77@hotmail.fr>
2022-05-27 11:28:36 +0200

    ajout dossier utils avec fichiers errors pour les gerer au signup

---
+++ backend/utils/errors.js
@@ -0,0 +1,23 @@
+/********************** GESTION DES ERREURS *******************/
+
+/*----- POUR LES ERREURS A L'INSCRIPTION -------*/
+module.exports.signUpErrors = (err) => {
+  let errors = { pseudo: "", email: "", password: "" }; // si les infos sont vides
+
+  if (err.message.includes("pseudo"))
+    errors.pseudo = "Pseudo incorrect ou déjà utilisé";
+
+  if (err.message.includes("email")) errors.email = "Email incorrect";
+
+  if (err.message.includes("password"))
+    errors.password = "Le mot de passe doit faire 6 caractères minimum";
+
+  if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("pseudo"))
+    // code 11000 en test postman
+    errors.pseudo = "Pseudo déjà utilisé";
+
+  if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("email"))
+    errors.email = "Cet email est déjà enregistré";
+
+  return errors;
+};
