a795e2f38672746076f4aefbceaf3869d42bd475
Aurelie Menin <atypik77@hotmail.fr>
2022-06-22 19:47:39 +0200

    customize navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,10 +1,22 @@
+/******************************* BARRE DE NAVIGATION **********************************/ 
+/*---------IMPORT----------*/
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <div>
-            NAVBAR
-        </div>
+        <nav>
+            <div className="nav-container">
+            <div className="logo">
+                    <NavLink exact="true" to="/">
+                        <div className="logo">
+                            <img src="./img/icon-left-font-monochrome-black.svg" alt="Logo Groupomania"/>
+                            {/* <h1>Groupomania</h1> */}
+                        </div>
+                    </NavLink>
+                </div>
+            </div>
+        </nav>
     );
 };
 
