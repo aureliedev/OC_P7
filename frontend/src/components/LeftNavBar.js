2468a769ef5f3395aa88bf4141fbead5afc7a589
Aurelie Menin <atypik77@hotmail.fr>
2022-06-23 17:06:02 +0200

    add LeftNavBar

---
+++ frontend/src/components/LeftNavBar.js
@@ -0,0 +1,17 @@
+/******************************* BARRE DE NAVIGATION GAUCHE **********************************/
+
+/*---------IMPORT----------*/ 
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+/*---------Component LeftNavBar----------*/
+const LeftNavBar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+/*---------EXPORT----------*/ 
+export default LeftNavBar;
