14b5afe16f50325894a54accf99a5daef9bf2d41
Aurelie Menin <atypik77@hotmail.fr>
2022-06-23 17:27:23 +0200

    useSelector for userData.pseudo work ok

--- frontend/src/components/Profil/UpdateProfil.js
+++ frontend/src/components/Profil/UpdateProfil.js
@@ -1,18 +1,20 @@
 /******************************* page UpdateProfil **********************************/
 
 /*---------IMPORT----------*/ 
-import React from 'react';
+import React, { useState } from 'react';
 import LeftNavBar from '../LeftNavBar';
+import { useSelector } from "react-redux"; /*récup' des data depuis le store*/
 
 
 /*---------Component UpdateProfil----------*/
 const UpdateProfil = () => {
-    
+    const userData = useSelector((state) => state.userReducer);
     
     /*---------Affichage FRONTENT----------*/
     return (
         <div className='profil-containeur'>
             <LeftNavBar />
+            <h1> Profil de {userData.pseudo} </h1>
         </div>
     );
 };
