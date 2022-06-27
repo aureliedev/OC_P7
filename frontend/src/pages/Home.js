3d05d2e59454cf938125282acc513281ca3aabc7
Aurelie Menin <atypik77@hotmail.fr>
2022-06-27 15:47:50 +0200

    mise en form du fil d'actu avec la navbargauche

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,13 @@
+/******************************* page HOME / FIL D'ACTUALITÉ **********************************/
+/*---------IMPORT----------*/
 import React from "react";
+import LeftNavBar from "../components/LeftNavBar";
 
 const Home = () => {
-  return <div>
-    hello depuis Home
+  return <div className="home">
+    <LeftNavBar />
     </div>;
 };
 
+/*---------EXPORT----------*/
 export default Home;
