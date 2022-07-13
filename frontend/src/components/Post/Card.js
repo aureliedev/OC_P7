a682a17a6d3863df76585165285db8ac98decec4
Aurelie Menin <atypik77@hotmail.fr>
2022-07-13 12:17:43 +0200

    work to img et pseudo affichage OK en front

--- frontend/src/components/Post/Card.js
+++ frontend/src/components/Post/Card.js
@@ -6,32 +6,40 @@ import { isEmpty, parserDate } from "../Utils";
 
 /*---------Fonction des cards----------*/
 const Card = ({ post }) => {
-  const [isLoading, setIsLoading] = useState(true); /* LOADING de chargement qui charge au debut(TRUE) */
-  const allUsersData = useSelector((state) => state.allUsersReducer); /* Pr avoir la data des utilisateurs */
+  const [isLoading, setIsLoading] =
+    useState(true); /* LOADING de chargement qui charge au debut(TRUE) */
+  const allUsersData = useSelector(
+    (state) => state.allUsersReducer
+  ); /* Pr avoir la data des utilisateurs */
   //const userData = useSelector((state) => state.userReducer); /* Pr avoir la data d'un utilisateur */
 
   useEffect(() => {
     !isEmpty(allUsersData[0]) &&
-      setIsLoading(false); /*Si la data est présente, on retire le LOADING de fontasome */
+      setIsLoading(
+        false
+      ); /*Si la data est présente, on retire le LOADING de fontasome */
   }, [allUsersData]);
 
   /* RENDU VISUEL FRONTEND */
   return (
     <li className="card-container" key={post._id}>
-      {isLoading ? /* isLoading est sur TRUE alors.. */ (
-        <i className="fas fa-spinner fa-spin"></i>
+      {isLoading ? (
+        /* isLoading est sur TRUE alors.. */ <i className="fas fa-spinner fa-spin"></i>
       ) : (
         <>
           <div className="card-left">
-            <img
+            <img /* Récup de la data des images */ 
               src={
-                !isEmpty(allUsersData[0]) && /* On verifie si la data est présente */
+                !isEmpty(
+                  allUsersData[0]
+                ) /* On verifie si la data est présente */ &&
                 allUsersData
-                  .map((user) => { /* map: pr chercher la photo de l'user */
+                  .map((user) => {
+                    /* map: pr chercher la photo de l'user */
                     if (user._id === post.posterId) {
                       return user.picture; /* Si photo trouvé, on l'affiche */
                     } else {
-                      return null;
+                      return null; /* Sinon stop */
                     }
                   })
                   .join("") /* string vide pour ne pas mettre de virgule entre chaque lettre */
@@ -39,20 +47,18 @@ const Card = ({ post }) => {
               alt="Avatar utilisateur"
             />
           </div>
-          <div className='card-right'>
-            <div className='card-header'>
-              <div className='pseudo'>
-                <h3>
-                {
-                                        !isEmpty(allUsersData[0]) &&
-                                        allUsersData
-                                            .map((users) => {
-                                                if (users._id === post.posterId)
-                                                    return users.pseudo
-                                                else return null
-                                            })
-                                            // .join('')
-                                    }
+          <div className="card-right">
+            <div className="card-header">
+              <div className="pseudo">
+                <h3> 
+                  { /* Récup du pseudo utilisateur */ 
+                    !isEmpty(allUsersData[0]) &&
+                      allUsersData.map((user) => {
+                        if (user._id === post.posterId) return user.pseudo;
+                        else return null;
+                      })
+                    // .join('')
+                  }
                 </h3>
               </div>
               <span>{parserDate(post.createdAt)}</span>
