8688f4380448ae28fbf28243a238047b5ae93943
Aurelie Menin <atypik77@hotmail.fr>
2022-07-15 21:19:18 +0200

    add logic for NewPostForm.js dans Homme

---
+++ frontend/src/components/Post/NewPostForm.js
@@ -0,0 +1,18 @@
+/******************************* FORMULAIRE DE NEW POST **********************************/
+/*---------IMPORT----------*/
+import React from 'react';
+
+/*---------fonction ormulaire de post----------*/
+const NewPostForm = () => {
+
+
+    /* RENDU VISUEL FRONTEND */
+    return (
+        <div>
+            FORM POST
+        </div>
+    );
+};
+
+/*---------EXPORT----------*/
+export default NewPostForm;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,15 +1,24 @@
 /******************************* page HOME **********************************/
 /*---------IMPORT----------*/
-import React from "react";
+import React, { useContext } from "react";
+import { UidContext } from "../components/AppContext";
 import LeftNavBar from "../components/LeftNavBar";
 import NewsFeed from "../components/NewsFeed";
+import NewPostForm from "../components/Post/NewPostForm";
 
 /*--------- fonction HOME (all actu) ----------*/
 const Home = () => {
+  const uid = useContext(UidContext); /*verifie si l'user est connecté */
+
+  /* RENDU VISUEL FRONTEND */
   return (
     <div className="home">
       <LeftNavBar />
       <div className="main">
+        <div className="home-header">
+          {uid ? <NewPostForm /> : null}
+        </div>
+        
         <NewsFeed />
       </div>
     </div>
