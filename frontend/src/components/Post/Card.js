400e661a87d60de3dfe1aeab93f9dd00ee937651
Aurelie Menin <atypik77@hotmail.fr>
2022-07-15 19:48:19 +0200

    work to edit post on Card.js

--- frontend/src/components/Post/Card.js
+++ frontend/src/components/Post/Card.js
@@ -7,12 +7,15 @@ import LikeButton from "./LikeButton";
 
 /*---------Fonction des cards----------*/
 const Card = ({ post }) => {
-  const [isLoading, setIsLoading] =
-    useState(true); /* LOADING de chargement qui charge au debut(TRUE) */
-  const allUsersData = useSelector(
-    (state) => state.allUsersReducer
-  ); /* Pr avoir la data des utilisateurs */
-  //const userData = useSelector((state) => state.userReducer); /* Pr avoir la data d'un utilisateur */
+  const [isLoading, setIsLoading] = useState(true); /* LOADING de chargement qui charge au debut(TRUE) */
+  const [isUpdated, setIsUpdated] = useState(false); /* Si chgt du CRUD */
+  const [textUpdate, setTextUpdate] = useState(null); /* Pr stoker le chgt de texte qui est NULL de base */
+  const allUsersData = useSelector((state) => state.allUsersReducer); /* Pr avoir la data des utilisateurs */
+  const userData = useSelector((state) => state.userReducer); /* Pr avoir la data d'un utilisateur */
+
+  const updateItem = () => { /* fonction pour mettre a jour le message du post */
+    
+  };
 
   useEffect(() => {
     !isEmpty(allUsersData[0]) &&
@@ -63,9 +66,29 @@ const Card = ({ post }) => {
               </div>
               <span>{parserDate(post.createdAt)}</span> {/* affichage de la date (fonction dans utils.js) */ }
             </div>
-            <p>{post.message} </p>
+            {isUpdated === false && <p>{post.message} </p>} {/* Si chgt alors tu affiches le boutton */}
+            {isUpdated && (
+              <div className="update-post"> 
+                <textarea
+                  defaultValue={post.message} /* Affiche le message initial */
+                  onChange={(e) => setTextUpdate(e.target.value)} /* A chaque changement on récup la value */
+                />
+                <div className="button-container">
+                  <button className="btn" onClick={updateItem}>
+                    Valider modifications
+                  </button>
+                </div>
+              </div>
+            )}
             {post.picture && (
               <img src={post.picture} alt="photographie de post" className="card-pic" />
+            )}
+            {userData._id === post.posterId && (
+              <div className="button-container">
+                <div onClick={() => setIsUpdated(!isUpdated)}> {/* !isUpdated: Pr ne plus afficher l'icone de modif post en recliquant dessus */}
+                  <img src="./img/icons/edit.svg" alt="éditer la publication" />
+                </div>
+              </div>
             )}
             <div className="card-footer"> 
               <LikeButton post={post} />
