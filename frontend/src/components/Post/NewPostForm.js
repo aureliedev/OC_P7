786adbe15e151a9c86f72bb402951419420ef863
Aurelie Menin <atypik77@hotmail.fr>
2022-07-15 21:57:24 +0200

    work to affichage du post form

--- frontend/src/components/Post/NewPostForm.js
+++ frontend/src/components/Post/NewPostForm.js
@@ -1,32 +1,49 @@
 /******************************* FORMULAIRE DE NEW POST **********************************/
 /*---------IMPORT----------*/
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { isEmpty } from '../Utils';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { isEmpty } from "../Utils";
 
-/*---------fonction ormulaire de post----------*/
+/*--------- FONCTION FORMULAIRE DE POST ----------*/
 const NewPostForm = () => {
-    const [isLoading, setIsLoading] = useState(true); /* Loader si connexion lente */
-    const [message, setMessage] = useState(""); /*Pr les messages de post */
-    const [postPicture, setPostPicture] = useState(null); /*Pr les images */
-    const [file, setFile] = useState(); /* */
-    const userData = useSelector((state) => state.userReducer); /* Pr aller cherhcer le contenu du store */
+  const [isLoading, setIsLoading] =
+    useState(true); /* Loader si connexion lente */
+  const [message, setMessage] = useState(""); /*Pr les messages de post */
+  const [postPicture, setPostPicture] = useState(null); /*Pr les images */
+  const [file, setFile] = useState(); /* */
+  const userData = useSelector((state) => state.userReducer); /* Pr aller cherhcer le contenu du store */
 
+  useEffect(() => {
+    if (!isEmpty(userData))
+      setIsLoading(false); /*Si le store a la date des users alors on passe le spinner sur FAlse */
+  }, [userData]); /* callback de la userData */
 
-    useEffect( () => {
-        if (!isEmpty(userData)) setIsLoading(false); /*Si le store a la date des users alors on passe le spinner sur FAlse */
-    }, [userData]);
-
-    /* RENDU VISUEL FRONTEND */
-    return (
-        <div className="post-container">
-            {isLoading ? (
-                <i className="fas fa-spinner fa-pulse"></i>
-            ) : ( 
-                <h2>LOOL </h2>
-            )}
+  /* RENDU VISUEL FRONTEND */
+  return (
+    <div className="post-container">
+      {isLoading ? (
+      <i className="fas fa-spinner fa-pulse"></i> 
+      ) : (
+      <>
+        <NavLink exact to="/profil"> {/* redirige vers le profil de l'user */}
+            <div className="user-info"> {/* Affiche l'img de l'user */}
+              <img src={userData.picture} alt="img profil utilisateur" />
+            </div>
+        </NavLink>
+        <div className="post-form">
+            <textarea 
+                name="message"
+                id="message"
+                placeholder="Quoi de neuf ?"
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+            />
         </div>
-    );
+      </>
+      )}
+    </div>
+  );
 };
 
 /*---------EXPORT----------*/
